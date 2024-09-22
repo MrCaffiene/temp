cc57e66a592379da0bed00ec295637f85fa32981
MrCaffiene <ankitdhotre21@gmail.com>
2024-09-22 23:18:53 +0530

    "modified"

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("Home Page");
 });
-server.listen(process.env.PORT || 3000, () => { console.log("server listening on port : 3000") });
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => { console.log("server listening on port : 3000") });
