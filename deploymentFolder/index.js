ba64a4eacc2f66be9da7c2eac11c01c71cd99437
MrCaffiene <ankitdhotre21@gmail.com>
2024-09-22 23:25:47 +0530

    tested code

--- deploymentFolder/index.js
+++ deploymentFolder/index.js
@@ -3,5 +3,5 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("Deployment is successful");
 });
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => { console.log("server listening on port : 3000") });
